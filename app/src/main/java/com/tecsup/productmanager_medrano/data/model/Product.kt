2d64decfb769ae1494ce1fa1cb149477ea01c98e
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-12-05 16:14:20 -0500

    Modelo Producto.

--- app/src/main/java/com/tecsup/productmanager_medrano/data/model/Product.kt
+++ app/src/main/java/com/tecsup/productmanager_medrano/data/model/Product.kt
@@ -1,2 +1,11 @@
 package com.tecsup.productmanager_medrano.data.model
 
+data class Producto(
+    val id: String = "",
+    val nombre: String = "",
+    val descripcion: String = "",
+    val precio: Double = 0.0,
+    val stock: Int = 0,
+    val urlImagen: String = "",
+    val idUsuario: String = "" 
+)
